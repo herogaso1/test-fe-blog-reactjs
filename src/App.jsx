493022d39d8046d30264919bf3b662c02d5622d1
Rabbit White <165736101+herogaso1@users.noreply.github.com>
2025-11-08 13:39:12 +0700

    refactor: correct import casing for Layout component and update CreateBlog route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 import Login from "./pages/Login";
-import BlogEdit from "./pages/BlogEdit";
 import SignUp from "./pages/SignUp/index.jsx";
+import CreateBlog from "./pages/CreateBlog";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/blog-edit" element={<BlogEdit />} />
+          <Route path="/CreateBlog" element={<CreateBlog />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
